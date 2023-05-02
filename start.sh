2bc073c9f19bc20af4931277cfdd6f2d4514512b
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-02 21:59:58 +0200

    correct script

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-tsch-ocherstra-make(){
+tschOcherstraMake(){
     # Compile TSCH-Orchestra folder
     cd tsch-orchestra && make 
 }
 
-tsch-orchestra-experiment-launch(){
+tschOrchestraExperimentLaunch(){
     # Run TSCH-Orchestra experiment
     cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra -d 10 -l strasbourg,m3,1,coordinator.iotlab -l strasbourg,m3,2,sender.iotlab
     iotlab-experiment wait
@@ -11,7 +11,7 @@ tsch-orchestra-experiment-launch(){
     return $id
 }
 
-plot-consumption(){
+plotConsumption(){
     # Get TSCH-Orchestra experiment
     id=$1
     path1="~/.iot-lab/$id/consumption/m3_1.oml"
@@ -20,6 +20,6 @@ plot-consumption(){
     plot_oml_consum -p -i $path2
 }
 
-tsch-ocherstra-make
-id=$(tsch-orchestra-experiment-launch)
-plot-consumption $id
+tschOcherstraMake
+id=$(tschOrchestraExperimentLaunch)
+plotConsumption $id
