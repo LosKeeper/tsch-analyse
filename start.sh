d30d2e8e669291d1b09f3f4ff558a456f6ffde14
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-03 22:38:35 +0200

    correct script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ tschOrchestraExperimentLaunch(){
     # Run TSCH-Orchestra experiment
     iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4
     iotlab-profile addm3 -n power_monitor_sender -voltage -current -power -period 8244 -avg 4
-    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra -d 10 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra -d 2 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender
     iotlab-experiment wait
     id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
     echo "Experiment launched with id : " $id
@@ -48,6 +48,6 @@ tschOcherstraMake
 tschOrchestraExperimentLaunch
 expId=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
 # Wait for the experiment to be launched and finished
-sleep 120
+sleep 180
 plotConsumption $expId
 plotRadio $expId
