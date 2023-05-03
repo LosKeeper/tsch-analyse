fab5bb3b0e87ef32c459714256cf360722ccf12c
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-03 13:59:09 +0200

    script monitor add

--- start.sh
+++ start.sh
@@ -6,10 +6,12 @@ tschOcherstraMake(){
 
 tschOrchestraExperimentLaunch(){
     # Run TSCH-Orchestra experiment
-    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra -d 20 -l strasbourg,m3,1,coordinator.iotlab -l strasbourg,m3,2,sender.iotlab
+    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4
+    iotlab-profile addm3 -n power_monitor_sender -voltage -current -power -period 8244 -avg 4
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra -d 20 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender
     iotlab-experiment wait
     local id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
-    echo $id
+    echo "Experiment launched with id : " $id
     cd ../../../..
     return $id
 }
