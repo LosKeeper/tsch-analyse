9ecc8497d13935387403e2c205fc2f2d9243a504
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-09 15:08:13 +0200

    correct print in script

--- start.sh
+++ start.sh
@@ -6,10 +6,10 @@ tschOcherstraMake(){
 
 tschOrchestraExperimentLaunchCoordSender(){
     # Run TSCH-Orchestra experiment with 1 sender and 1 coordinator
-    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4
-    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-1send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1
-    iotlab-experiment wait
+    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-1send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1 > /dev/null
+    iotlab-experiment wait > /dev/null
     id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
     echo "Experiment with 1 sender and 1 coordinator launched with id : " $id
     cd ../../../..
@@ -18,11 +18,11 @@ tschOrchestraExperimentLaunchCoordSender(){
 
 tschOrchestraExperimentLaunchCoordSender2(){
     # Run TSCH-Orchestra experiment with 2 senders and 1 coordinator
-    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender2 -voltage -current -power -period 8244 -avg 4
-    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-2send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,power_monitor_sender2
-    iotlab-experiment wait
+    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender2 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-2send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,power_monitor_sender2 > /dev/null
+    iotlab-experiment wait > /dev/null
     id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
     echo "Experiment with 2 senders and 1 coordinator launched with id : " $id
     cd ../../../..
@@ -31,19 +31,19 @@ tschOrchestraExperimentLaunchCoordSender2(){
 
 tschOrchestraExperimentLaunchCoordSender10(){
     # Run TSCH-Orchestra experiment with 10 senders and 1 coordinator
-    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender2 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender3 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender4 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender5 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender6 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender7 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender8 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender9 -voltage -current -power -period 8244 -avg 4
-    iotlab-profile addm3 -n power_monitor_sender10 -voltage -current -power -period 8244 -avg 4
-    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-10send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,power_monitor_sender2 -l strasbourg,m3,4,sender.iotlab,power_monitor_sender3 -l strasbourg,m3,5,sender.iotlab,power_monitor_sender4 -l strasbourg,m3,6,sender.iotlab,power_monitor_sender5 -l strasbourg,m3,7,sender.iotlab,power_monitor_sender6 -l strasbourg,m3,8,sender.iotlab,power_monitor_sender7 -l strasbourg,m3,9,sender.iotlab,power_monitor_sender8 -l strasbourg,m3,10,sender.iotlab,power_monitor_sender9 -l strasbourg,m3,11,sender.iotlab,power_monitor_sender10
-    iotlab-experiment wait
+    iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender1 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender2 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender3 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender4 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender5 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender6 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender7 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender8 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender9 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    iotlab-profile addm3 -n power_monitor_sender10 -voltage -current -power -period 8244 -avg 4 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-10send-1coord -d 5 -l strasbourg,m3,1,coordinator.iotlab,power_monitor_coord -l strasbourg,m3,2,sender.iotlab,power_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,power_monitor_sender2 -l strasbourg,m3,4,sender.iotlab,power_monitor_sender3 -l strasbourg,m3,5,sender.iotlab,power_monitor_sender4 -l strasbourg,m3,6,sender.iotlab,power_monitor_sender5 -l strasbourg,m3,7,sender.iotlab,power_monitor_sender6 -l strasbourg,m3,8,sender.iotlab,power_monitor_sender7 -l strasbourg,m3,9,sender.iotlab,power_monitor_sender8 -l strasbourg,m3,10,sender.iotlab,power_monitor_sender9 -l strasbourg,m3,11,sender.iotlab,power_monitor_sender10 > /dev/null
+    iotlab-experiment wait > /dev/null
     id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
     echo "Experiment with 10 senders and 1 coordinator launched with id : " $id
     cd ../../../..
