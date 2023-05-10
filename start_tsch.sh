245b7eb5ab166639c233ee24d4eff577a7bc2120
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-10 17:22:25 +0200

    correct script

--- start_tsch.sh
+++ start_tsch.sh
@@ -143,6 +143,7 @@ plotConsumption(){
     id=$1
     # plot all the consumption
     for i in {1..$2}
+    do
         pathi=~/.iot-lab/$id/consumption/m3_$i.oml
         plot_oml_consum -p -i $pathi
     done
@@ -153,6 +154,7 @@ plotRadio(){
     id=$1
     # plot all the radio
     for i in {1..$2}
+    do
         pathi=~/.iot-lab/$id/radio/m3_$i.oml
         plot_oml_radio -a -i $pathi
     done
