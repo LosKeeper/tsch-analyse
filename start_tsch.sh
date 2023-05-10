d4699afa7787f9871a5cf22cb3c967cd4874265c
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-10 09:18:59 +0200

    adding radio tests

--- start_tsch.sh
+++ start_tsch.sh
@@ -16,6 +16,18 @@ tschOrchestraExperimentLaunchCoordSender1(){
     return $id
 }
 
+tschOrchestraExperimentLaunchCoordSender1Radio(){
+    # Run TSCH-Orchestra experiment with 1 sender and 1 coordinator
+    iotlab-profile addm3 -n radio_monitor_coord -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender1 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-1send-1coord-radio -d 5 -l strasbourg,m3,1,coordinator.iotlab,radio_monitor_coord -l strasbourg,m3,2,sender.iotlab,radio_monitor_sender1 > /dev/null
+    iotlab-experiment wait > /dev/null
+    id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
+    echo "Experiment with 1 sender and 1 coordinator launched with id for radio : " $id
+    cd ../../../..
+    return $id
+}
+
 tschOrchestraExperimentLaunchCoordSender2(){
     # Run TSCH-Orchestra experiment with 2 senders and 1 coordinator
     iotlab-profile addm3 -n power_monitor_coord -voltage -current -power -period 8244 -avg 4 > /dev/null
@@ -25,6 +37,19 @@ tschOrchestraExperimentLaunchCoordSender2(){
     iotlab-experiment wait > /dev/null
     id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
     echo "Experiment with 2 senders and 1 coordinator launched with id : " $id
+    cd ../../../..
+    return $id
+}
+
+tschOrchestraExperimentLaunchCoordSender2Radio(){
+    # Run TSCH-Orchestra experiment with 2 senders and 1 coordinator
+    iotlab-profile addm3 -n radio_monitor_coord -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender1 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender2 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-2send-1coord-radio -d 5 -l strasbourg,m3,1,coordinator.iotlab,radio_monitor_coord -l strasbourg,m3,2,sender.iotlab,radio_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,radio_monitor_sender2 > /dev/null
+    iotlab-experiment wait > /dev/null
+    id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
+    echo "Experiment with 2 senders and 1 coordinator launched with id for radio : " $id
     cd ../../../..
     return $id
 }
@@ -48,6 +73,24 @@ tschOrchestraExperimentLaunchCoordSender10(){
     echo "Experiment with 10 senders and 1 coordinator launched with id : " $id
     cd ../../../..
     return $id
+}
+
+tschOrchestraExperimentLaunchCoordSender10Radio(){
+    iotlab-profile addm3 -n radio_monitor_coord -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender1 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender2 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender3 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender4 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender5 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender6 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender7 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender8 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender9 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    iotlab-profile addm3 -n radio_monitor_sender10 -rssi -channels 11 14 -rperiod 1 -num 1 > /dev/null
+    cd tsch-orchestra/build/iotlab/m3 && iotlab-experiment submit -n tsch-orchestra-10send-1coord-radio -d 5 -l strasbourg,m3,1,coordinator.iotlab,radio_monitor_coord -l strasbourg,m3,2,sender.iotlab,radio_monitor_sender1 -l strasbourg,m3,3,sender.iotlab,radio_monitor_sender2 -l strasbourg,m3,4,sender.iotlab,radio_monitor_sender3 -l strasbourg,m3,5,sender.iotlab,radio_monitor_sender4 -l strasbourg,m3,6,sender.iotlab,radio_monitor_sender5 -l strasbourg,m3,7,sender.iotlab,radio_monitor_sender6 -l strasbourg,m3,8,sender.iotlab,radio_monitor_sender7 -l strasbourg,m3,9,sender.iotlab,radio_monitor_sender8 -l strasbourg,m3,10,sender.iotlab,radio_monitor_sender9 -l strasbourg,m3,11,sender.iotlab,radio_monitor_sender10 > /dev/null
+    iotlab-experiment wait > /dev/null
+    id=$(iotlab-experiment get -p | grep id | cut -d' ' -f6 | cut -d',' -f1)
+    echo "Experiment with 10 senders and 1 coordinator launched with id for radio : " $id
 }
 
 plotConsumption(){
@@ -79,6 +122,6 @@ plotRadio(){
 }
 
 tschOcherstraMake
-tschOrchestraExperimentLaunchCoordSender1
-tschOrchestraExperimentLaunchCoordSender2
-tschOrchestraExperimentLaunchCoordSender10
+tschOrchestraExperimentLaunchCoordSender1Radio
+tschOrchestraExperimentLaunchCoordSender2Radio
+tschOrchestraExperimentLaunchCoordSender10Radio
