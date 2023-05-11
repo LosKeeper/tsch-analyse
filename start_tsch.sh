f3e536790deb5e465fa29537c1782c556d237fda
LosKeeper <thomas.dumond2001@gmail.com>
2023-05-11 17:01:01 +0200

    adding log

--- start_tsch.sh
+++ start_tsch.sh
@@ -1,6 +1,6 @@
 tschOcherstraMake(){
     # Compile TSCH-Orchestra folder
-    cd tsch-orchestra && make
+    cd tsch-orchestra && make clean && make
     cd ..
 }
 
@@ -34,8 +34,8 @@ tschOrchestraExperimentLaunchCoordSender1Log(){
     idExp=`iotlab-experiment submit -n tsch-orchestra-1send-1coord-log -d 5 -l strasbourg,m3,1,coordinator.iotlab -l strasbourg,m3,2,sender.iotlab | grep "id" | cut -d' ' -f6 | cut -d' ' -f1`
     iotlab-experiment wait -i $idExp > /dev/null
     echo "Experiment with 1 sender and 1 coordinator launched with id for log : " $idExp
-    serial_aggregator -i $idExp > ~.iot-lab/$idExp/serial_aggregator.log
     cd ../../../..
+    serial_aggregator -i $idExp > $idExp.log
     return $idExp
 }
 
