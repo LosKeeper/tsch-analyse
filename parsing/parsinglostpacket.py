1a14ab1dbf899627e74a5be8679a14db20962360
CIPOLLA NICOLAS <nicolas.cipolla@etu.unistra.fr>
2023-05-12 13:54:45 +0200

    adding the parsing function for CSMA

--- parsing/parsinglostpacket.py
+++ parsing/parsinglostpacket.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def countlostpacket(numlog):
+def countlostpacketTSCH(numlog):
     filename="assets/"+str(numlog)+".log"
     f=open(filename,"r")
 
@@ -13,22 +13,60 @@ def countlostpacket(numlog):
             countreceive+=1
 
     lostpacket=countsend-countreceive
-    return lostpacket,countsend
+    return lostpacket,countsend,countreceive
 
-print(countlostpacket(369622))
-print(countlostpacket(369629))
-print(countlostpacket(369648))
+def countlostpacketCSMA(numlog):
+    filename="assets/"+str(numlog)+".log"
+    f=open(filename,"r")
+
+    countsend=0
+    countreceive=0
+    for line in f.readlines():
+        if(line.find("sending")!=-1 or line.find("Sending")!=-1):
+            countsend+=1
+        elif(line.find("received")!=-1 or line.find("Received")!=-1):
+            countreceive+=1
+
+    lostpacket=countsend-countreceive
+    return lostpacket,countsend,countreceive
+
+print("TSCH")
+print(countlostpacketTSCH(369622))
+print(countlostpacketTSCH(369629))
+print(countlostpacketTSCH(369648))
+
 xpoints=[1,2,10]
-ypointssend=[countlostpacket(369622)[1],countlostpacket(369629)[1],countlostpacket(369648)[1]]
-ypointslostpacket=[countlostpacket(369622)[0],countlostpacket(369629)[0],countlostpacket(369648)[0]]
+ypointssend=[countlostpacketTSCH(369622)[1],countlostpacketTSCH(369629)[1],countlostpacketTSCH(369648)[1]]
+ypointslostpacket=[countlostpacketTSCH(369622)[0],countlostpacketTSCH(369629)[0],countlostpacketTSCH(369648)[0]]
+
 plt.plot(xpoints,ypointssend)
-plt.title("Number of send packet depending on number of sender")
+plt.title("TSCH : Number of send packet depending on number of sender")
 plt.xlabel("Number of sender")
 plt.ylabel("Number of send packet")
 plt.show()
 
 plt.plot(xpoints,ypointslostpacket)
-plt.title("Number of lost packet depending on number of sender")
+plt.title("TSCH : Number of lost packet depending on number of sender")
+plt.xlabel("Number of sender")
+plt.ylabel("Number of lost packet")
+plt.show()
+
+print("CSMA")
+print(countlostpacketCSMA(369653))
+print(countlostpacketCSMA(369659))
+print(countlostpacketCSMA(369820))
+
+ypointssendCSMA=[countlostpacketCSMA(369653)[1],countlostpacketCSMA(369659)[1],countlostpacketCSMA(369820)[1]]
+ypointslostpacketCSMA=[countlostpacketCSMA(369653)[0],countlostpacketCSMA(369659)[0],countlostpacketCSMA(369820)[0]]
+
+plt.plot(xpoints,ypointssendCSMA)
+plt.title("CSMA : Number of send packet depending on number of sender")
+plt.xlabel("Number of sender")
+plt.ylabel("Number of send packet")
+plt.show()
+
+plt.plot(xpoints,ypointslostpacketCSMA)
+plt.title("CSMA : Number of lost packet depending on number of sender")
 plt.xlabel("Number of sender")
 plt.ylabel("Number of lost packet")
 plt.show()
